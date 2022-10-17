71ef6e2b2d3b531527ddbc8898b4bee273045d66
Anand310 <anandcs310@gmail.com>
2022-10-17 18:37:36 +0530

    Initial commit

---
+++ src/test/java/com/dailycodebuffer/Spring/Security/Demo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.dailycodebuffer.Spring.Security.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
